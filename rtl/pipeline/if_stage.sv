9dd5edbce59f611694ee0486122cf17cd1668b68
CHIRANJEET1729DAS <126919390+CHIRANJEET1729DAS@users.noreply.github.com>
2025-01-13 14:59:10 +0530

    Add files via upload

---
+++ rtl/pipeline/if_stage.sv
@@ -0,0 +1,39 @@
+module fetch_stage(input wire clk , input wire [15:0] cache_address , output reg [15:0] instruction_register_address , output reg [15:0] data_out);
+  always @(posedge clk)begin
+	  data_out <= cache_address;
+	  instruction_register_address <= data_out;
+  end
+endmodule
+
+module tb;
+   // input 
+  reg clk;
+  reg [15:0] cache_address;
+   //output
+  wire [15:0] instruction_register_address;  
+  wire [15:0] data_out;
+  
+  fetch_stage uut(.clk(clk),.cache_address(cache_address),.instruction_register_address(instruction_register_address),.data_out(data_out));
+
+   //clk intitialisation
+  always begin
+        #5 clk = ~clk; 
+  end
+  
+  initial begin	  
+   //initiate the signals 
+   clk = 0;
+   cache_address = 16'b0;
+   
+
+   //hold for a period
+   #10
+   
+   //implement the first operation
+   cache_address = 16'h12_12;
+   $display("the data fetched and passed for the next state : : %1h",data_out);
+   $finish;
+  end
+endmodule
+
+  
