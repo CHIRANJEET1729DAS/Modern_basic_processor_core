7f89184e8c5a36caaec2ba36118b49697de0ec11
CHIRANJEET1729DAS <126919390+CHIRANJEET1729DAS@users.noreply.github.com>
2025-01-14 15:56:03 +0530

    Update id_stage.sv

--- rtl/pipeline/id_stage.sv
+++ rtl/pipeline/id_stage.sv
@@ -1,4 +1,4 @@
-module decode(input wire clk,input wire [31:0] instruction,input wire [31:0] instruction_register,output reg [4:0] operand_1,output reg [4:0] operand_2,output reg [4:0] destination_reg,output reg [3:0] alu_control,output reg reg_write,output reg [15:0] immediate_value); 
+module decode(input wire clk,input wire [31:0] instruction,input wire [31:0] general_purpose_register,output reg [4:0] operand_1,output reg [4:0] operand_2,output reg [4:0] gpr_destination_address,output reg [3:0] alu_control,output reg reg_write,output reg [15:0] immediate_value); 
 
 // Decoding the instruction
 wire [5:0] opcode = instruction[31:26];
@@ -8,29 +8,29 @@ wire [4:0] rd = instruction[15:11];
 wire [15:0] immediate = instruction[15:0];
 
 // Combinational logic
-always @(opcode or instruction_register[rs] or instruction_register[rt] or instruction_register[rd] or immediate) begin
+always @(opcode or general_purpose_register[rs] or general_purpose_register[rt] or gpr_destination_address[rd] or immediate) begin
     case(opcode)
         6'b000000: begin // R type instruction (like add, subtract, division etc)
             alu_control <= 4'b0001;
-            operand_1 <= instruction_register[rs];
-            operand_2 <= instruction_register[rt];
-            destination_reg <= rd;
+            operand_1 <= general_purpose_register[rs];
+            operand_2 <= general_purpose_register[rt];
+            gpr_destination_address <= rd;
             immediate_value <= 16'b0;
             reg_write <= 1'b1;
         end
         6'b000001: begin // I-type instruction (addi)
             alu_control <= 4'b0010;
-            operand_1 <= instruction_register[rs];
-            operand_2 <= instruction_register[rt];
-            destination_reg <= rt; // destination should be rt for I-type instructions
+            operand_1 <= general_purpose_register[rs];
+            operand_2 <= general_purpose_register[rt];
+            gpr_destination_address <= rt; // destination should be rt for I-type instructions
             immediate_value <= immediate;
             reg_write <= 1'b1;
         end
         6'b000010: begin // J-type instruction format
             alu_control <= 4'b0000;
-            operand_1 <= instruction_register[rs];
-            operand_2 <= instruction_register[rt];
-            destination_reg <= rd; // destination is rd for J-type (but may be unused)
+            operand_1 <= general_purpose_register[rs];
+            operand_2 <= general_purpose_register[rt];
+            gpr_destination_address <= rd; // destination is rd for J-type (but may be unused)
             immediate_value <= 16'b0;
             reg_write <= 1'b1;
         end
@@ -43,32 +43,32 @@ module tb;
 //input parameters
 
 reg clk;
-reg[31:0] instruction_register;
+reg[31:0] general_purpose_register;
 reg[31:0] instruction;
 
 //output parameters
 
 wire[4:0] operand_1;
 wire[4:0] operand_2;
-wire[4:0] destination_reg;
+wire[4:0] gpr_destination_address;
 wire[3:0] alu_control;
 wire reg_write;
 wire[15:0] immediate_value;
 
 //load the parameters to the module 
-decode uut(.clk(clk),.instruction_register(instruction_register),.instruction(instruction),.operand_1(operand_1),.operand_2(operand_2),.destination_reg(destination_reg),.reg_write(reg_write),.immediate_value(immediate_value));
+decode uut(.clk(clk),.general_purpose_register(general_purpose_register),.instruction(instruction),.operand_1(operand_1),.operand_2(operand_2),.gpr_destination_address(gpr_destination_address),.reg_write(reg_write),.immediate_value(immediate_value));
 always begin 
  #5 clk = ~clk;
 end 
 initial begin
 	//intitialize the signal 
 	 clk = 0;
-	 instruction_register = 32'b0;
+	 general_purpose_register = 32'b0;
 	 instruction = 32'b0;
 	//hold
 	 #10
 	//operation
-	 instruction_register = 32'h12_13;
+	 general_purpose_register = 32'h12_13;
 	 instruction = 32'h342;
          $display("the instruction has be decoded");
 	 $finish;
