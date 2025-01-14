549416869d9932dce8264228d87e1153816ec83d
CHIRANJEET1729DAS <126919390+CHIRANJEET1729DAS@users.noreply.github.com>
2025-01-14 15:56:32 +0530

    Update if_stage.sv

--- rtl/pipeline/if_stage.sv
+++ rtl/pipeline/if_stage.sv
@@ -1,7 +1,7 @@
-module fetch_stage(input wire clk , input wire [15:0] cache_address , output reg [15:0] instruction_register_address , output reg [15:0] data_out);
+module fetch_stage(input wire clk , input wire [15:0] cache_address , output reg [15:0] general_purpose_register , output reg [15:0] data_out);
   always @(posedge clk)begin
 	  data_out <= cache_address;
-	  instruction_register_address <= data_out;
+	  general_purpose_register <= data_out;
   end
 endmodule
 
@@ -10,10 +10,10 @@ module tb;
   reg clk;
   reg [15:0] cache_address;
    //output
-  wire [15:0] instruction_register_address;  
+  wire [15:0] general_purpose_register;  
   wire [15:0] data_out;
   
-  fetch_stage uut(.clk(clk),.cache_address(cache_address),.instruction_register_address(instruction_register_address),.data_out(data_out));
+  fetch_stage uut(.clk(clk),.cache_address(cache_address),.general_purpose_register(general_purpose_register),.data_out(data_out));
 
    //clk intitialisation
   always begin
