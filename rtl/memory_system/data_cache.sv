25e8712eea9a241959d183b9e11b4824b4998c45
CHIRANJEET1729DAS <126919390+CHIRANJEET1729DAS@users.noreply.github.com>
2025-01-16 12:06:28 +0530

    Add files via upload

---
+++ rtl/memory_system/data_cache.sv
@@ -0,0 +1,28 @@
+module data_cache(input wire clk,input wire reset,input wire[1023:0] data_cache,input wire[31:0] data,output wire index_for_quick_access);
+
+reg hit ;
+integer index_for_cache = 0;
+
+always @(posedge clk or posedge reset)begin
+         if (reset)begin
+            hit <= 0;
+            index_for_cache <= 0;
+         end
+         else begin
+            for (index = 0; index<32;index=index+1)begin
+                 if (data_cache[index*32+:32] == data)begin
+                        hit <= 1'b0;
+                        index_for_quick_access <= index*32;
+                 end
+
+                 if (~hit)begin
+                        data_cache[index_for_cache*32+:32] = data;
+                        index_for_quick_access <= index+32;
+                        index_for_cache = index_for_cache + 1;
+                 end
+            end
+          end
+end
+endmodule
+
+
